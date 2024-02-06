0629bdfaace05de92eab02bc2bc7d1e0fcbcb790
genexyz <gianlucapagano14@gmail.com>
2024-02-06 18:57:14 -0300

    Improved 404 page + style fixes

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -11,6 +11,14 @@ const NotFound = () => {
       <h1 className={styles.title}>404</h1>
       <h2 className={styles.subtitle}>{t("notFound.title")}</h2>
       <p className={styles.description}>{t("notFound.description")}</p>
+      <div className={styles.tooltipContainer}>
+        <h3 className={styles.tooltipTitle}>{t("tooltip.title")}</h3>
+        <h4>{t("tooltip.citySearch")}</h4>
+        <p>{t("tooltip.cityTooltipText")}</p>
+        <p>{t("tooltip.cityUSStateTooltipText")}</p>
+        <h4>{t("tooltip.zipSearch")}</h4>
+        <p>{t("tooltip.zipTooltipText")}</p>
+      </div>
       <Link href="/" passHref>
         <button className={styles.backHomeButton}>
           {t("notFound.goBack")}
